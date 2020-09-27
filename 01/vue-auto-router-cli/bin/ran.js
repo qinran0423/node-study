2608401c79b73d07be9350f0d8db4189356f9382
randy0423 <1156107187@qq.com>
2020-09-27 22:55:50 +0800

    手写脚手架 安装依赖

--- 01/vue-auto-router-cli/bin/ran.js
+++ 01/vue-auto-router-cli/bin/ran.js
@@ -7,6 +7,8 @@ program.version(require('../package.json').version)
 
 program.command('init <name>')
   .description('init project')
+  // .action(name => {
+  //   console.log('init ' + name);
+  // })
   .action(require('../lib/init'))
-
 program.parse(process.argv)
