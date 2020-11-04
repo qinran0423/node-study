82a5e89d4d8d2adce5da796e549b91a9448c5370
RandyQin <randyqin@haizol.com>
2020-11-04 11:06:23 +0800

    跨域

--- 03/http/api.js
+++ 03/http/api.js
@@ -4,26 +4,26 @@ http
   .createServer((req, res) => {
     const { method, url } = req;
     console.log('url:', url, method);
-    console.log(req.headers.cookie);
+    console.log('cookie', req.headers.cookie);
     if (method == "GET" && url == "/") {
       fs.readFile("./index.html", (err, data) => {
         res.setHeader("Content-Type", "text/html");
         res.end(data);
       });
     } else if (method == "GET" && url == "/api/users") {
+      console.log('get');
       res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
       res.setHeader("Content-Type", "application/json");
       res.setHeader('Access-Control-Allow-Credentials', 'true');
-      res.setHeader('Set-Cookie', 'cookie1=123')
+      res.setHeader('Set-Cookie', 'cookie1=123;')
       res.end(JSON.stringify([{ name: "tom", age: 20 }]));
     } else if (method === 'OPTIONS' && url === '/api/users') {
       res.setHeader('Access-Control-Allow-Credentials', 'true');
-
-      res.writeHead(200, {
-        "Access-Control-Allow-Origin": "http://localhost:3000",
-        "Access-Control-Allow-Headers": "X-Token,Content-Type",
-        "Access-Control-Allow-Methods": "PUT"
-        })
+      res.writeHead(200,  {
+        'Access-Control-Allow-Origin' : 'http://localhost:3000',
+        'Access-Control-Allow-Headers' : 'X-Token,Content-Type',
+        'Access-Control-Allow-Methods': 'PUT',
+      })
       res.end()
     }
   })
